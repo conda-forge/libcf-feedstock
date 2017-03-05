cbd957c5c96adb0660b109ae90329b37885ed2ea
Denis <nadeau1@llnl.gov>
2017-03-05 00:23:07 -0800

    add source activate

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,21 @@
+
 source activate "${CONDA_DEFAULT_ENV}"
-export CFLAGS="-Wall -m64 -pipe -O2  -fPIC ${CFLAGS}"
+
+export CFLAGS="-Wall -m64 -pipe -O2 -fPIC ${CFLAGS}"
 export CXXFLAGS="${CFLAGS} ${CXXFLAGS}"
 export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS}"
 export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
 export LFLAGS="-fPIC ${LFLAGS}"
 
+echo ${CFLAGS}
+echo ${CPPFLAGS}
+echo ${LDFLAGS}
+echo ${LFLAGS}
+echo ${CC}
+
 
 ./configure --prefix=${PREFIX}
+
 ${PYTHON} setup.py install
+
 if [ `uname` == Darwin ]; then install_name_tool -change /System/Library/Frameworks/Python.framework/Versions/2.7/Python @rpath/libpython2.7.dylib ${SP_DIR}/pycf/*.so ; fi
