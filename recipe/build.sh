a43008be6f14856940abd2ac97bc074e988deda7
Xylar Asay-Davis <xylarstorm@gmail.com>
2021-07-13 17:35:00 +0200

    Update build to copy configs needed by ARM OSX

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
 export CFLAGS="-Wall -m64 -pipe -O2  -fPIC -I${PREFIX}/include ${CFLAGS}"
 export CXXFLAGS="${CFLAGS} ${CXXFLAGS}"
 export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS}"
